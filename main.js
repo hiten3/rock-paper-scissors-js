008167ae0b22182054689fe239ef73f963a7a7ff
Hiten <hitz1993@gmail.com>
2019-02-18 21:22:35 +0000

    add game()

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
-function computerPlay () {
+function computerPlay() {
     // create random number from 1 - 3
     let randNum = Math.floor(Math.random() * 3 + 1);
-    
+
     switch (randNum) {
         case 1:
             return "ROCK"
@@ -20,19 +20,61 @@ function playRound(playerSelection, computerSelection) {
     // remove case sensitivity
     playerSelection = playerSelection.toUpperCase();
 
+    // Player Draw
     if (playerSelection === computerSelection) {
-        return `You both have ${playerSelection}, it's a Draw.`
-    } 
-    else if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS')
-    || (playerSelection === 'PAPER' && computerSelection === 'ROCK')
-    || (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')) {
-        return `You Win! ${playerSelection} beats ${computerSelection}.`
+        // return `You both have ${playerSelection}. It's a Draw.`
+        return 'DRAW'
     }
+    // Player Win
+    else if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS')
+        || (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+        || (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')) {
+        //return `You Win! ${playerSelection} beats ${computerSelection}.`
+        return 'WIN'
+    }
+    // Player Lose
     else {
-        return `You lose ${computerSelection} beats ${playerSelection}.`
+        // return `You lose ${computerSelection} beats ${playerSelection}.`
+        return 'LOSE'
     }
 }
 
-const playerSelection = 'rock'
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
+let playerWins = 0;
+let computerWins = 0;
+
+function game() {
+
+
+    let playerSelection = prompt("Rock, Paper or Scissors?", `${computerPlay()}`)
+    const computerSelection = computerPlay();
+
+    function roundCount(roundResult) {
+        switch (roundResult) {
+            case 'WIN':
+                playerWins++;
+                console.log(`You win this round! ${playerSelection} beats ${computerSelection}.`);
+                break;
+            case 'LOSE':
+                computerWins++;
+                console.log(`You win this round! ${playerSelection} beats ${computerSelection}.`);
+                break;
+
+            case 'DRAW':
+                console.log(`You both have ${playerSelection}. It's a Draw.`);
+                break;
+            default:
+                break;
+        }
+    }
+
+    roundCount(playRound(playerSelection, computerSelection));
+    console.log(`Player Win: ${playerWins} Lose: ${computerWins}`)
+
+
+}
+
+game();
+game();
+game();
+game();
+game();
