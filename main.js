9fcd77919fad70d979a47deea55d8af423152973
Hiten <hitz1993@gmail.com>
2019-02-18 19:30:24 +0000

    add playRound()

--- main.js
+++ main.js
@@ -5,13 +5,34 @@ function computerPlay () {
     
     switch (randNum) {
         case 1:
-            return "Rock"
+            return "ROCK"
             break;
         case 2:
-            return "Paper"
+            return "PAPER"
             break;
         case 3:
-            return "Scissors"
+            return "SCISSORS"
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    // remove case sensitivity
+    playerSelection = playerSelection.toUpperCase();
+
+    if (playerSelection === computerSelection) {
+        return `You both have ${playerSelection}, it's a Draw.`
+    } 
+    else if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS')
+    || (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    || (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')) {
+        return `You Win! ${playerSelection} beats ${computerSelection}.`
+    }
+    else {
+        return `You lose ${computerSelection} beats ${playerSelection}.`
+    }
+}
+
+const playerSelection = 'rock'
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
